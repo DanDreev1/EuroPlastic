35c953b3df7b19c92cb5cb7dd7bc3dc334d66a14
Daniil Andrieiev <daniilandreev788@gmail.com>
2023-10-25 19:33:06 +0100

    new_update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-document.getElementById("lang").addEventListener("click", function() {
-    let target = document.getElementById("hide__lang");
-    let nav = document.getElementById("nav");
-    if (target.style.display === "inline-block") {
-        nav.style.padding = "25px 0";
-        target.style.display = "none"; // Hide the element
-    } else {
-        nav.style.padding = "25px 0 70px";
-        target.style.display = "inline-block"; // Show the element
-    }
-});
-
 // Get all FAQ question buttons and corresponding text elements
 const questionButtons = document.querySelectorAll('.faq__question');
 const textElements = document.querySelectorAll('.faq__text');
@@ -32,15 +20,5 @@ questionButtons.forEach((button, index) => {
                 text.style.display = 'none';
             }
         });
-    });
-});
-
-// Get a reference to the Contact button and the footer
-const buttons = document.querySelectorAll('.btn__contact');
-const contact = document.getElementById('contacts');
-
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        contact.scrollIntoView({ behavior: 'smooth' });
     });
 });
